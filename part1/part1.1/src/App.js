a559d0a91e59d8a9fa0913153be8620ba21ffc33
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2022-10-26 00:19:57 +0200

    Update App.js

--- part1/part1.1/src/App.js
+++ part1/part1.1/src/App.js
@@ -1,11 +1,11 @@
 const App = () => {
-    const course = 'Half stack application development';
-    const part1 = 'Fundamentals of React';
-    const exercises1 = 10;
-    const part2 = 'Using props to pass data';
-    const exercises2 = 7;
-    const part3 = 'State of a component';
-    const exercises3 = 14;
+    const course = 'Half stack application development'
+    const part1 = 'Fundamentals of React'
+    const exercises1 = 10
+    const part2 = 'Using props to pass data'
+    const exercises2 = 7
+    const part3 = 'State of a component'
+    const exercises3 = 14
 
     return (
         <div>
@@ -21,13 +21,13 @@ const App = () => {
 const Header = (props) => {
     return (
         <h1>{props.course}</h1>
-    );
+    )
 }
 
 const Content = (props) => {
     return (
         <p>{props.part}: {props.exercises}</p>
-    );
+    )
 }
 
 const Total = (props) => {
@@ -36,4 +36,4 @@ const Total = (props) => {
     )
 }
 
-export default App;
+export default App

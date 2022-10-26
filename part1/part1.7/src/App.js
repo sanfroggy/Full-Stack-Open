713bd280410ef2c665c84b43cd922f25a8e8c533
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2022-10-26 04:49:18 +0200

    Add files via upload

---
+++ part1/part1.7/src/App.js
@@ -0,0 +1,55 @@
+import { useState } from 'react'
+
+const App = () => {
+    //Save the states of each button
+    const [good, setGood] = useState(0)
+    const [neutral, setNeutral] = useState(0)
+    const [bad, setBad] = useState(0)
+    const [all, setAll] = useState(0)
+    const [average, setAverage] = useState(0)
+    const [percentage, setPercentage] = useState(0)
+
+    //Add a good feedback and calculate the average value and the percentage of positive feedback
+    const handleGood = () => {
+        setGood(good + 1)
+        setAll(all + 1)
+        setAverage(((good + 1) - bad) / (all + 1))
+        setPercentage((good + 1) / (all + 1))
+    }
+
+    //Add a neutral feedback and calculate the average value and the percentage of positive feedback
+    const handleNeutral = () => {
+        setNeutral(neutral + 1)
+        setAll(all + 1)
+        setAverage((good - bad) / (all + 1))
+        setPercentage(good / (all + 1))
+    }
+
+    //Add a bad feedback and calculate the average value and the percentage of positive feedback
+    const handleBad = () => {
+        setBad(bad + 1)
+        setAll(all + 1)
+        setAverage(((good) - (bad + 1)) / (all + 1))
+        setPercentage(good / (all + 1))
+    }
+
+    //Handle the click events for each button and show feedback statistics
+    return (
+        <div>
+            <h1>Give feedback</h1>
+            <button onClick={handleGood}> Good</button>
+            <button onClick={handleNeutral}> Neutral</button>
+            <button onClick={handleBad}>Bad</button>
+            <h1>Staticstics</h1>
+            <p>Good: {good}</p>
+            <p>Neutral: {neutral}</p>
+            <p>Bad: {bad}</p>
+            <p>All: {all}</p>
+            <p>Average: {average}</p>
+            <p>Positive: {percentage} %</p>
+        </div>
+    )
+
+}
+
+export default App

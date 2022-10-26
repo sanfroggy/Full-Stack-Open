ca1d002726aa88e9d1738f9cb0328dc58d3602d5
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2022-10-26 02:44:50 +0200

    Update App.js

--- part1/part1.6/src/App.js
+++ part1/part1.6/src/App.js
@@ -5,17 +5,14 @@ const App = () => {
     const [good, setGood] = useState(0)
     const [neutral, setNeutral] = useState(0)
     const [bad, setBad] = useState(0)
-
-    const handleGood = () => setGood(good + 1)
-    const handleNeutral = () => setNeutral(neutral + 1)
-    const handleBad = () => setBad(bad + 1)
-
+    
+    //Handle the click events for eacg button and show feedback statistics
     return (
         <div>
             <h1>Give feedback</h1>
-            <button onClick={handleGood}> Good</button>
-            <button onClick={handleNeutral}> Neutral</button>
-            <button onClick={handleBad}>Bad</button>
+            <button onClick={() => setGood(good + 1)}> Good</button>
+            <button onClick={() => setNeutral(neutral + 1)}> Neutral</button>
+            <button onClick={() => setBad(bad + 1)}>Bad</button>
             <h1>Staticstics</h1>
             <p>Good: {good}</p>
             <p>Neutral: {neutral}</p>

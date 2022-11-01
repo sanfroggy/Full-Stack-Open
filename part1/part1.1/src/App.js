80ae61c1af4aee498b39f1de53aaa95169264532
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2022-11-01 05:07:16 +0200

    Update App.js

--- part1/part1.1/src/App.js
+++ part1/part1.1/src/App.js
@@ -1,4 +1,5 @@
 const App = () => {
+    //Defining the constants to be passed to different components
     const course = 'Half stack application development'
     const part1 = 'Fundamentals of React'
     const exercises1 = 10
@@ -8,6 +9,7 @@ const App = () => {
     const exercises3 = 14
 
     return (
+        //Displaying the contents of components
         <div>
             <Header course={course} />
             <Content part={part1} exercises={exercises1} />
@@ -18,18 +20,21 @@ const App = () => {
     )
 }
 
+//Defining the Header component
 const Header = (props) => {
     return (
         <h1>{props.course}</h1>
     )
 }
 
+//Defining the Content component
 const Content = (props) => {
     return (
         <p>{props.part}: {props.exercises}</p>
     )
 }
 
+//Defining the Total component
 const Total = (props) => {
     return (
         <p>Number of exercises: {props.firstnum + props.secondnum + props.thirdnum}</p>

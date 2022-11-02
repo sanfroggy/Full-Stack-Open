2932b65e0b3031ecd824dd0abe4c9bc26e41771f
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2022-11-02 05:09:15 +0200

    Update App.js

--- part1/part1.4/src/App.js
+++ part1/part1.4/src/App.js
@@ -1,4 +1,6 @@
 const App = () => {
+     /*Defining the parts of the course as an object. The properties of this object are to be
+    passed to different components as well as the name of the course*/
     const course = 'Half stack application development'
     const parts = [
         {
@@ -16,6 +18,7 @@ const App = () => {
     ]
 
     return (
+        //Displaying the contents of components
         <div>
             <Header course={course} />
             <Content parts={parts}/>
@@ -24,12 +27,14 @@ const App = () => {
     )
 }
 
+//Defining the Header component
 const Header = (props) => {
     return (
         <h1>{props.course}</h1>
     )
 }
 
+//Defining the Content component that uses three Part components to display data
 const Content = (props) => {
     return (
         <>
@@ -40,12 +45,14 @@ const Content = (props) => {
     )
 }
 
+//Defining the Part component
 const Part = (props) => {
     return (
         <p>{props.part}: {props.exercises}</p>
     )
 }
 
+//Defining the Total component
 const Total = (props) => {
     return (
         <p>Number of exercises: {props.parts[0].exercises + props.parts[1].exercises + props.parts[2].exercises}</p>

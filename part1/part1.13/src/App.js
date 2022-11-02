01f5832b031aaaf715c10225ecb5f3e31e3d33bb
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2022-11-02 06:00:14 +0200

    Update App.js

--- part1/part1.13/src/App.js
+++ part1/part1.13/src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 
 const App = () => {
+    
+    //Define an array of anecdotes
     const anecdotes = [
         'If it hurts, do it more often.',
         'Adding manpower to a late software project makes it later!',
@@ -11,16 +13,21 @@ const App = () => {
         'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
     ]
 
+    //Define a variable to randomize also the initial anecdote
     let random = Math.floor(Math.random() * anecdotes.length)
 
+    //Save the state of the index of the selected anecdote and the given votes
     const [selected, setSelected] = useState(random)
     const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
 
+    /*Set the index of the selected state to the redefined random variable, 
+    when the button is pressed*/
     const displayRandom = () => {
         random = Math.floor(Math.random() * anecdotes.length)
         setSelected(random)
     }
 
+    //Create a copy of the array of votes and add 1 vote for the selected anecdote
     const addVote = () => {
         const copy = [...votes]
         copy[selected] += 1
@@ -28,6 +35,8 @@ const App = () => {
     }
 
     return (
+        /*Handle button click events and display the random anecdote,
+        as well as votes for the current anecdote*/
         <>
             <p>{anecdotes[selected]}</p>
             <p>(Votes: {votes[selected]})</p>

41625863b3c7880b0046b7d0e02fb44adf5a201c
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2022-11-02 06:10:01 +0200

    Finished anecdotes app

--- part1/part1.14/src/App.js
+++ part1/part1.14/src/App.js
@@ -1,6 +1,8 @@
 import { useState } from 'react'
 
 const App = () => {
+    
+    //Define an array of anecdotes
     const anecdotes = [
         'If it hurts, do it more often.',
         'Adding manpower to a late software project makes it later!',
@@ -11,17 +13,25 @@ const App = () => {
         'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.'
     ]
 
+    //Define a variable to randomize also the initial anecdote
     let random = Math.floor(Math.random() * anecdotes.length)
 
+    /*Save the state of the index of the selected anecdote, given votes 
+    and the index of the most voted anecdote*/
     const [selected, setSelected] = useState(random)
     const [votes, setVotes] = useState(new Array(anecdotes.length).fill(0))
     const [mostvoted, setMostVoted] = useState(0)
 
+    /*Set the index of the selected state to the redefined random variable, 
+    when the button is pressed*/
     const displayRandom = () => {
         random = Math.floor(Math.random() * anecdotes.length)
         setSelected(random)
     }
 
+    /*Create a copy of the array of votes, add 1 vote for the selected anecdote
+    and set the most voted anecdote.(If two or more are equally voted, then
+    which ever index is first in the array is displayed.)*/
     const addVote = () => {
         const copy = [...votes]
         copy[selected] += 1
@@ -30,6 +40,9 @@ const App = () => {
     }
 
     return (
+        /*Handle button click events and display the random anecdote,
+        as well as votes for the current anecdote and 
+        the most voted anecdote inside it's own component*/
         <>
             <h1>Anecdote of the day:</h1>
             <p>{anecdotes[selected]}</p>
@@ -41,6 +54,7 @@ const App = () => {
     )
 }
 
+//Define the MostVotedAnecdote component
 const MostVotedAnecdote = (props) => {
     return (
         <>

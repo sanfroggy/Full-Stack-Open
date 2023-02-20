fd815726b7a795732da5257b293bb64bab0866de
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-02-20 05:15:55 +0200

    Update App.js

--- part2/part2.1/src/App.js
+++ part2/part2.1/src/App.js
@@ -3,18 +3,22 @@ const App = () => {
    passed to the Course component*/
     const course = {
         name: 'Half stack application development',
+	id: 1,
         parts: [
             {
                 name: 'Fundamentals of React',
-                exercises: 10
+                exercises: 10,
+	    	id: 1
             },
             {
                 name: 'Using props to pass data',
-                exercises: 7
+                exercises: 7,
+		id: 2
             },
             {
                 name: 'State of a component',
-                exercises: 14
+                exercises: 14,
+		id: 3
             }
         ]
     }

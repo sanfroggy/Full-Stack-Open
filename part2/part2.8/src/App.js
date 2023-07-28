9635088811259bcc8de30c060306a99ca2504534
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-07-28 13:07:34 +0300

    Update App.js

--- part2/part2.8/src/App.js
+++ part2/part2.8/src/App.js
@@ -5,7 +5,7 @@ import Person from './components/Person'
 const App = (props) => {
 
     /*Creating "state variables" for the person object to be stored
-    and the name currently written on the input field */
+    and the values currently written on the input fields */
     const [persons, setPersons] = useState([
         { name: 'Arto Hellas', number: '+358 45 2235689' }
     ])
@@ -37,11 +37,13 @@ const App = (props) => {
         setNewName(event.target.value)
     }
 
+    /*Getting the value of the input field when it is changed
+    and setting it as a new value for the newNumber "state variable". */
     const handleNumberInputChange = (event) => {
         setNewNumber(event.target.value)
     }
 
-    //Displaying the form components as well as the names in the persons Array.
+    //Displaying the form components as well as the names and numbers in the persons Array.
     return (
         <div>
             <h2>Phonebook</h2>

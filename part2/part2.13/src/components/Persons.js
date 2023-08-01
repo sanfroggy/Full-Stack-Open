c1f2e1f279ab0a3164bc670eb63802e79d77d811
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-08-01 06:59:19 +0300

    Add files via upload

---
+++ part2/part2.13/src/components/Persons.js
@@ -0,0 +1,17 @@
+//Defining the Persons component containing a list of persons.
+const Persons = (props) => {
+    return (
+        props.contactList.map(person =>
+            <Person key={person.name} name={person.name} number={person.number} />
+        )
+    )
+}
+
+//Defining the Personcomponent containing the information of a single contact.
+const Person = (props) => {
+    return (
+        <p>{props.name}: {props.number}</p>
+    )
+}
+
+export default Persons

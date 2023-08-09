b2420cc0f8eab21114cf950b7c0ad8b32b49e3d8
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-08-09 05:36:15 +0300

    Add files via upload

---
+++ part2/part2.14/src/components/Persons.js
@@ -0,0 +1,22 @@
+//Defining the Persons component containing a list of persons.
+const Persons = (props) => {
+    return (
+        props.contactList.map(person =>
+            <Person contact={person} key={person.name} name={person.name} number={person.number}
+                deleteFnct={props.deleteFunction} />
+        )
+    )
+}
+
+/*Defining the Person component containing the information of a single contact 
+as well as a button and a related function for deleting the Person in question 
+from the list of contacts displayed. */
+const Person = (props) => {
+
+    return (
+        <p>{props.name}: {props.number}  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+            <button onClick={() => { props.deleteFnct(props.contact.id, props.name) }}>Delete</button></p>
+    )
+}
+
+export default Persons

07a75842d46ffa6f73fdd1dec5a48e197910c5dc
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-08-09 05:21:19 +0300

    Update Persons.js

--- part2/part2.13/src/services/Persons.js
+++ part2/part2.13/src/services/Persons.js
@@ -2,14 +2,17 @@ import axios from 'axios'
 
 const baseUrl = 'http://localhost:3001/persons'
 
+//Creating a function to return all contacts from JSON server.
 const getAll = () => {
     const request = axios.get(baseUrl)
     return request.then(response => response.data)
 }
 
+//Creating a function to add a new contact to JSON server.
 const createContact = newContact => {
     const request = axios.post(baseUrl, newContact)
     return request.then(response => response.data)
 }
 
+// eslint-disable-next-line import/no-anonymous-default-export
 export default { getAll, createContact }

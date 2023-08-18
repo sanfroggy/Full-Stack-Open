87065e4bda4017f3bd5af8fbebba681ad48ed994
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-08-18 04:14:41 +0300

    Update Notification.js

--- part2/part2.16/src/components/Notification.js
+++ part2/part2.16/src/components/Notification.js
@@ -1,7 +1,8 @@
 import '../index.css'
 
 /*Defining the Notification component containing a message to be displayed,
-when an operation is completed and adding a css-style to the div element. */
+when an operation is completed and setting a css-style to the div element. 
+The 2 usable styles are defined in the imported index.css file*/
 const Notification = ({ message }) => {
 
     if (message === null) {

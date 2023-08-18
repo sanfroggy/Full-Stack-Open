e167108e4425ae751b152a58cc2b6ee1126175f4
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-08-18 04:14:01 +0300

    Finished phonebook app.

--- part2/part2.17/src/components/Notification.js
+++ part2/part2.17/src/components/Notification.js
@@ -1,8 +1,9 @@
 import '../index.css'
 
 /*Defining the Notification component containing a message to be displayed,
-when an operation is completed and adding a css-style to the div element
-according to the value of received isError. */
+when an operation is completed and setting a css-style to the div element
+according to the value of received isError. The 2 usable styles are
+defined in the imported index.css file */
 const Notification = ({ message, isError }) => {
 
     if (message === null) {

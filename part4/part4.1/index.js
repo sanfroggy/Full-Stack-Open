be63dbfbb01bf336bcc147eac2fd293abe1fc056
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-09-25 08:09:25 +0300

    Part 4.1

--- part4/part4.1/index.js
+++ part4/part4.1/index.js
@@ -1,9 +1,11 @@
+//Defining the constants for the use of Express, cors, and mongoose.
 const http = require('http')
 const express = require('express')
 const app = express()
 const cors = require('cors')
 const mongoose = require('mongoose')
 
+/*Define a schema to use as a model for a Blog object to be saved to MongoDB. */
 const blogSchema = mongoose.Schema({
     title: String,
     author: String,
@@ -12,13 +14,18 @@ const blogSchema = mongoose.Schema({
 })
 
 const Blog = mongoose.model('Blog', blogSchema)
+
+/*Define a password constant given as argument on startup as well
+and use it to from a url to connect to. */
 const pwd = encodeURIComponent(process.argv[2])
 const mongoUrl = `mongodb+srv://San:${pwd}@cluster0.od5jh2q.mongodb.net/blogListApp?retryWrites=true&w=majority`
 mongoose.connect(mongoUrl)
 
+//Define the use of cors and express json parser middlewares.
 app.use(cors())
 app.use(express.json())
 
+//Define the routes for getting blogs from MongoDB and saving a new one.
 app.get('/api/blogs', (request, response) => {
     Blog
         .find({})
@@ -37,6 +44,7 @@ app.post('/api/blogs', (request, response) => {
         })
 })
 
+//Define a port to listen to.
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)

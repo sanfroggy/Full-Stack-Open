9791502cb172863f95f086cf9183730b68608aef
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-09-25 22:20:14 +0300

    Add files via upload

---
+++ part4/part4.3/tests/dummy.test.js
@@ -0,0 +1,9 @@
+const listHelper = require('../utils/list_helper')
+
+//Test if the dummy function of listHelper returns number one.
+test('dummy returns one', () => {
+  const blogs = []
+
+  const result = listHelper.dummy(blogs)
+  expect(result).toBe(1)
+})
---
+++ part4/part4.3/utils/list_helper.js
@@ -0,0 +1,9 @@
+/*Create and export a dummy function to test the 
+test configuration. */
+const dummy = (blogs) => {
+    return 1
+}
+
+module.exports = {
+    dummy
+}

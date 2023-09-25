e401cf4575bfe123ae88bdcbf1a75243f78b5687
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-09-25 22:59:51 +0300

    Update list_helper.js

--- part4/part4.4/utils/list_helper.js
+++ part4/part4.4/utils/list_helper.js
@@ -4,6 +4,8 @@ const dummy = (blogs) => {
     return 1
 }
 
+/*Create and export a totalLikes function to calculate the
+total number of likes in a received array of blogs. */
 const totalLikes = (blogs) => {
     const total = blogs.reduce((a, b) => a + b.likes, 0)
     return total

cdffe6de1d79c171d366d8915f02fc3f54f0ed31
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-09-26 08:22:41 +0300

    Update blog_api.test.js

--- part4/part4.8/tests/blog_api.test.js
+++ part4/part4.8/tests/blog_api.test.js
@@ -1,9 +1,14 @@
+/*Defining constants for mongoose, super test and app 
+module to be used as "superagent". */
 const mongoose = require('mongoose')
 const supertest = require('supertest')
 const app = require('../app')
 
 const api = supertest(app)
 
+/*Creating a test using async and await to test that
+the correct number of blogs is returned from MongoDB
+database. */
 test('correct number of blogs is returned', async () => {
 
     const response = await api.get('/api/blogs')
@@ -11,6 +16,7 @@ test('correct number of blogs is returned', async () => {
     expect(response.body).toHaveLength(3)
 })
 
+//Closing the MongoDB connection.
 afterAll(async () => {
     mongoose.connection.close()
 })

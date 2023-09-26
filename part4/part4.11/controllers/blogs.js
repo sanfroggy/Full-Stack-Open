6897d212b6fdd6a50f7aa9d33d4316db17bf96d2
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-09-26 19:12:26 +0300

    Add files via upload

---
+++ part4/part4.11/controllers/blogs.js
@@ -0,0 +1,26 @@
+//Define constants for express Router and the blog module.
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+
+/*Define the routes for getting blogs from MongoDB and saving a new one
+with async/await. */
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({})
+    response.json(blogs)
+})
+
+blogsRouter.post('/', async (request, response) => {
+    const body = request.body
+
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes  || 0
+    })
+
+    const savedBlog = await blog.save()
+    response.status(201).json(savedBlog)
+})
+
+module.exports = blogsRouter
---
+++ part4/part4.11/tests/blog_api.test.js
@@ -0,0 +1,105 @@
+/*Defining constants for mongoose, super test and app 
+module to be used as "superagent". */
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+const api = supertest(app)
+const Blog = require('../models/blog')
+
+/*Creating an array of blogs to initialize the MongoDB
+database with. */
+const initialBlogs = [
+    {
+        title: 'My fullstack studies',
+        author: 'San',
+        url: 'http://www.mydevblog.com',
+        likes: 30502
+    },
+    {
+
+        title: 'Crazy for cinema',
+        author: 'FilmFanatic',
+        url: 'http://www.allaboutthatav.org',
+        likes: 126345
+    },
+    {
+        title: 'Philosophical corner',
+        author: 'NewSocrates',
+        url: 'http://www.ithinkthereforeiam.org',
+        likes: 1345
+    }
+]
+
+/*Clearing the MongoDB database and inserting initialBlogs
+array into it. */
+beforeEach(async () => {
+    await Blog.deleteMany({})
+    await Blog.insertMany(initialBlogs)
+
+})
+
+/*Creating a test using async and await to test that
+the correct number of blogs is returned from MongoDB
+database. */
+test('correct number of blogs is returned', async () => {
+
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(3)
+})
+
+/*Creating a test using async and await to test that
+a id field exists for each of the blogs instead of an
+_id field. */
+test('the idenfitying field for a blog is id and not _id', async () => {
+
+    const response = await api.get('/api/blogs')
+
+    response.body.forEach(blog => {
+        expect(blog.id).toBeDefined()
+    })
+})
+
+/*Creating a test using async and await to test that
+a new blog is added to the database when a post request is
+sent and that the data is in json form. */
+test('blogs can be added to the database successfully', async () => {
+
+    const comparisonResponse = await api.get('/api/blogs')
+    const originalLength = comparisonResponse.body.length
+
+    const newBlog = {
+        title: 'A blog about software testing',
+        author: 'San',
+        url: "http://www.mytestblog.com",
+        likes: 3560
+    }
+
+    await api.post('/api/blogs').send(newBlog)
+        .expect(201)
+        .expect('Content-Type', /application\/json/)
+
+    const newResponse = await api.get('/api/blogs')
+
+    expect(newResponse.body.length).toBe(originalLength + 1)
+
+})
+
+test('if the added blog is not given a value for likes it is set to zero', async () => {
+
+    const newBlog = {
+        title: 'A blog no one likes',
+        author: 'Unknown',
+        url: "http://www.conspiracytheorist.net"
+    }
+
+    const response = await api.post('/api/blogs').send(newBlog)
+    expect(response.body.likes).toBe(0)
+    console.log(response.body)
+
+})
+
+//Closing the MongoDB connection.
+afterAll(async () => {
+    mongoose.connection.close()
+})

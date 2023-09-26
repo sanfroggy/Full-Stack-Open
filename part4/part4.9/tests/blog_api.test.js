7235dddc9cca5ddaafc4efcac6a1669f9b8717a9
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-09-26 08:33:19 +0300

    Update blog_api.test.js

--- part4/part4.9/tests/blog_api.test.js
+++ part4/part4.9/tests/blog_api.test.js
@@ -16,6 +16,9 @@ test('correct number of blogs is returned', async () => {
     expect(response.body).toHaveLength(3)
 })
 
+/*Creating a test using async and await to test that
+a id field exists for each of the blogs instead of an
+_id field. */
 test('the idenfitying field for a blog is id and not _id', async () => {
 
     const response = await api.get('/api/blogs')

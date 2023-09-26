574b53bfacfcc689d74387d0189195c1963f10a4
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-09-26 18:35:08 +0300

    Add files via upload

--- part4/part4.8/controllers/blogs.js
+++ part4/part4.8/controllers/blogs.js
@@ -2,25 +2,23 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
 
-/*Define the routes for getting blogs from MongoDB and saving a new one
-with async/await. */
+/*Define the routes for getting blogs from MongoDB with async/await
+and saving a new one. */
 blogsRouter.get('/', async (request, response) => {
     const blogs = await Blog.find({})
     response.json(blogs)
 })
 
-blogsRouter.post('/', async (request, response) => {
-    const body = request.body
+blogsRouter.post('/', (request, response, next) => {
+    const blog = new Blog(request.body)
 
-    const blog = new Blog({
-        title: body.title,
-        author: body.author,
-        url: body.url,
-        likes: body.likes
-    })
-
-    const savedBlog = await blog.save()
-    response.status(201).json(savedBlog)
+    blog
+        .save()
+        .then(result => {
+            response.status(201).json(result)
+        })
+        .catch(error => next(error))
 })
+
 
 module.exports = blogsRouter

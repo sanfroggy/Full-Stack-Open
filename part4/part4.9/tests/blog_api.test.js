185fe89a1669036000222d8faa58fc0cbaf9945d
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-09-26 08:31:33 +0300

    Add files via upload

---
+++ part4/part4.9/tests/blog_api.test.js
@@ -0,0 +1,31 @@
+/*Defining constants for mongoose, super test and app 
+module to be used as "superagent". */
+const mongoose = require('mongoose')
+const supertest = require('supertest')
+const app = require('../app')
+
+const api = supertest(app)
+
+/*Creating a test using async and await to test that
+the correct number of blogs is returned from MongoDB
+database. */
+test('correct number of blogs is returned', async () => {
+
+    const response = await api.get('/api/blogs')
+
+    expect(response.body).toHaveLength(3)
+})
+
+test('the idenfitying field for a blog is id and not _id', async () => {
+
+    const response = await api.get('/api/blogs')
+
+    response.body.forEach(blog => {
+        expect(blog.id).toBeDefined()
+    })  
+})
+
+//Closing the MongoDB connection.
+afterAll(async () => {
+    mongoose.connection.close()
+})

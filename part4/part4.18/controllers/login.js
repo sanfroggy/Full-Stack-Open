8069a07357a98e429bd019c976bbc6bfca4f562a
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-09-27 23:44:22 +0300

    Update login.js

--- part4/part4.18/controllers/login.js
+++ part4/part4.18/controllers/login.js
@@ -1,8 +1,15 @@
+/*Defining the use of jsonwebtoken, bcrypt, express router
+and necessary modules. */
 const jwt = require('jsonwebtoken')
 const bcrypt = require('bcrypt')
 const loginRouter = require('express').Router()
 const User = require('../models/user')
 
+/*Defining the route for logging in using token-based
+authentication. the existence of a User with the received username
+is checked and the bcrypt is used to compare the hash of the given
+password to the hash saved in MongoDB database. Environmental variable
+SECRET is also used when generating a token. */
 loginRouter.post('/', async (request, response) => {
     const { username, password } = request.body
 

bdd21e55668a7bd068ee1ae8ae2ae3287f6bb4f2
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-09-27 04:46:31 +0300

    Update users.js

--- part4/part4.15/controllers/users.js
+++ part4/part4.15/controllers/users.js
@@ -1,8 +1,10 @@
+//Define constants for bcrypt, express Router and the user module.
 const bcrypt = require('bcrypt')
-const User = require('../models/user')
 const usersRouter = require('express').Router()
-const user = require('../models/user')
+const User = require('../models/user')
 
+/*Define the route for saving a new user with async / await, 
+and use bcrypt to hash the given password. */
 usersRouter.post('/', async (request, response) => {
     const { username, name, password } = request.body
 
@@ -20,6 +22,7 @@ usersRouter.post('/', async (request, response) => {
     response.status(201).json(savedUser)
 })
 
+//Define the route for getting users from MongoDB.
 usersRouter.get('/', async (request, response) => {
     const users = await User.find({})
     response.json(users)

11da76bc9087643eb7a21216a45e0672199a91b8
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-09-29 05:08:40 +0300

    Add files via upload

---
+++ part4/part4.22/controllers/blogs.js
@@ -0,0 +1,114 @@
+/*Defining constants for jwt, blog model,
+express Router, blog and user modules and 
+userExtractor middleware. */
+//const jwt = require('jsonwebtoken')
+const blog = require('../models/blog')
+const blogsRouter = require('express').Router()
+const Blog = require('../models/blog')
+const User = require('../models/user')
+const { userExtractor } = require('../utils/middleware')
+
+
+/*Defining the route for getting blogs from MongoDB and
+populating their user value with the referred User object's
+username, and name. */
+blogsRouter.get('/', async (request, response) => {
+    const blogs = await Blog.find({}).populate('user', {username: 1, name: 1})
+    response.json(blogs)
+})
+
+/*Defining the route for saving a new blog with async / await, 
+unless title or url have undefined or null values. Also decoding
+the received autorization token to identify the logged in user. */
+blogsRouter.post('/', userExtractor, async (request, response) => {
+    const body = request.body
+
+    const user = request.user
+
+    /*Defining the new blog object and giving it a
+    a user._id value to refer to the user who created it. */
+    const blog = new Blog({
+        title: body.title,
+        author: body.author,
+        url: body.url,
+        likes: body.likes || 0,
+        user: user._id
+    })
+
+    /*Saving the blog._id in the blog collection of the
+    user as well and saving the user to the database
+    with the blog._id defined. */
+    if (!blog.title || !blog.url) {
+        response.status(400).end()
+    } else {
+        const savedBlog = await blog.save()
+        if (user.blogs.length === 0) {
+            user.blogs = user.blogs[0] = savedBlog._id
+            await user.save()
+        } else {
+            user.blogs = user.blogs.concat(savedBlog._id)
+            await user.save()
+        }        
+
+        response.status(201).json(savedBlog)
+    }
+
+})
+
+/*Defining the route for deleting an existing blog with
+async / await, unless the given id is invalid. */
+blogsRouter.delete('/:id', userExtractor, async (request, response) => {
+
+    /*Decoding the received authorization token to identify the logged in user
+    and making sure that only the user who has created the blog,
+    can delete it. */
+    blogToDelete = await Blog.findById(request.params.id)
+
+    const user = request.user
+
+    if (user.id.toString() !== blogToDelete.user.toString()) {
+        return response.status(401).json({ error: 'Unauthorized.' })
+    }
+
+    if (blogToDelete !== null && blogToDelete !== undefined) {
+
+        await Blog.findOneAndRemove(blogToDelete)
+
+        /*Filtering out the id references from the user's blog list, 
+        in which the object referred to does not exist. */
+        user.blogs = await user.blogs.filter(blog => Blog.findById(blog.id) !== null)
+            .filter(blog => Blog.findById(blog.id) !== undefined)
+
+        await user.save()
+        
+        response.status(204).end()
+    } else {
+        response.status(400).end()
+    }
+})
+
+/*Defining the route for updating an existing blog with
+async / await, unless the given id is invalid. */
+blogsRouter.put('/:id', async (request, response) => {
+
+    blogToUpdate = await Blog.findById(request.params.id)
+
+    if (blogToUpdate !== null && blogToUpdate !== undefined) {
+        const body = request.body
+
+        const blog = {
+            title: body.title,
+            author: body.author,
+            url: body.url,
+            likes: body.likes
+        }
+
+        const updatedBlog = await Blog.findByIdAndUpdate(
+            request.params.id, blog, { new: true })
+        response.status(200).json(updatedBlog)
+    } else {
+        response.status(404).end()
+    }
+})
+
+module.exports = blogsRouter
---
+++ part4/part4.22/models/blog.js
@@ -0,0 +1,31 @@
+//Defining the constants for mongoose and User.
+const mongoose = require('mongoose')
+const User = require('./user')
+
+/*Define a schema to use as a model for a Blog object 
+to be saved to MongoDB, as well as a refence to User
+object that created the Blog object in question. */
+const blogSchema = new mongoose.Schema({
+    title: String,
+    author: String,
+    url: String,
+    likes: Number,
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User'
+    }
+})
+
+/*Define the properties of the objects that are returned by the toJSON method.
+Exclude the _id value as well as the MongoDB version field __v.
+Also transform the value of _id from object to a string */
+blogSchema.set("toJSON", {
+    transform: (document, returnedObject) => {
+        returnedObject.id = returnedObject._id.toString()
+        delete returnedObject._id
+        delete returnedObject.__v
+    }
+})
+
+//Export the Blog object model.
+module.exports = mongoose.model('Blog', blogSchema)

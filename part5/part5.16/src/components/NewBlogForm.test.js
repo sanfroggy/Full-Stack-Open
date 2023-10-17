ccfa4db3fbd233a2cd2afd02652d255c178677b1
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-10-17 05:48:59 +0300

    Update NewBlogForm.test.js

--- part5/part5.16/src/components/NewBlogForm.test.js
+++ part5/part5.16/src/components/NewBlogForm.test.js
@@ -6,6 +6,8 @@ import { render, screen } from '@testing-library/react'
 import userEvent from '@testing-library/user-event'
 import NewBlogForm from './NewBlogForm'
 
+/*Testing that when the blog creation form is filled in and "Create" is
+clicked a mock function is called and the callback function has the correct data. */
 describe('When a new Blog object is created', () => {
     test('the callback function received is called with the proper data', async () => {
 
@@ -16,6 +18,7 @@ describe('When a new Blog object is created', () => {
         const user = userEvent.setup()
         const button = screen.getByText('Create')
 
+        //Filling in the form and clicking "Create".
         const title = screen.getByPlaceholderText('Enter blog title here.')
         const author = container.querySelector('#authorInput')
         const url = screen.getByPlaceholderText('Enter blog url here.')

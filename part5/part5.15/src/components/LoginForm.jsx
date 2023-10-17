8b58ae4acc8d8d5fe201a7643fb79dff09e6a893
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-10-17 04:13:24 +0300

    Add files via upload

---
+++ part5/part5.15/src/components/LoginForm.jsx
@@ -0,0 +1,41 @@
+//Importing prop-types
+import PropTypes from 'prop-types'
+
+/*Defining a LoginForm component with text and password type input
+fields and a submit button. The required methods and data are received
+from props. */
+const LoginForm = ({
+    loginMethod,
+    handleUsrInputChangeMethod,
+    handlePwdInputChangeMethod,
+    usr,
+    pwd
+}) => {
+    return (
+        <form onSubmit={loginMethod}>
+            <h2>Log in to application: </h2>
+            <div>
+                Username: <input type="text" value={usr} onChange={handleUsrInputChangeMethod} />
+            </div>
+            <br></br>
+            <div>
+                Password: <input type="password" value={pwd} onChange={handlePwdInputChangeMethod} />
+            </div>
+            <br></br>
+            <div>
+                <button type="submit">Login</button>
+            </div>
+        </form>
+    )
+}
+
+//Using prop-types to give a warning if necessary values are not received correctly.
+LoginForm.propTypes = {
+    loginMethod: PropTypes.func.isRequired,
+    handleUsrInputChangeMethod: PropTypes.func.isRequired,
+    handlePwdInputChangeMethod: PropTypes.func.isRequired,
+    usr: PropTypes.string.isRequired,
+    pwd: PropTypes.string.isRequired
+}
+
+export default LoginForm

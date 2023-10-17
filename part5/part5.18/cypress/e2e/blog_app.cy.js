5997545cd6ce59502c986d7d06a3f6b54f171c5a
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-10-18 01:21:16 +0300

    Add files via upload

---
+++ part5/part5.18/cypress/e2e/blog_app.cy.js
@@ -0,0 +1,73 @@
+describe('When blog app frontend is visited', function () {
+
+    /*Emptying the database from User and Blog objects and
+    visiting the "http://localhost:3000" defined in the configuration
+    file as baseUrl, before each test. Also creating a new user to test
+    logging in. */
+    beforeEach(function () {
+        cy.request('POST', `${Cypress.env('BACKEND')}/testing/reset`)
+
+        const user = {
+            username: 'SanTheBlogger',
+            name: 'Sampson',
+            password: 'Salasana92'
+        }
+
+        cy.request('POST', `${Cypress.env('BACKEND')}/users`, user)
+        cy.visit('/')
+    })
+
+    /*Testing that the login form is shown by default when the frontend
+    url is visited. */
+    it('login form is shown by default', function () {
+        cy.contains('Login')
+        cy.contains('Log in to application:')
+        cy.contains('Username:')
+        cy.contains('Password:')
+    })
+
+    describe('When user attempts to login', function () {
+        /*Testing that an existing user can login via the custom command
+        connecting directly to the backend. */
+        it('user can login with correct credentials via custom command', function () {
+            cy.contains('Login')
+            cy.login({ username: 'SanTheBlogger', password: 'Salasana92' })
+
+            cy.contains('Currently logged in as Sampson.')
+
+        })
+
+        it('user cannot login with incorrect credentials via custom command', function () {
+
+            cy.loginWithResponse({ username: 'SanTheBlogger', password: 'V‰‰r‰Salis' }).should( 'eq' , 401 )
+            cy.contains('Currently logged in as Sampson.').should('not.exist')
+
+        })
+
+        /*Testing that an existing user can login via the login form. */
+        it('user can login with correct credentials via login form',
+            function () {
+                cy.contains('Login')
+                cy.get('#usrInput').type('SanTheBlogger')
+                cy.get('#pwdInput').type('Salasana92')
+                cy.get('#loginBtn').click()
+                cy.contains('Currently logged in as Sampson.')
+            })
+
+        /*Testing that an existing user can login via the login form and
+        that invalid user data produces a failed login attempt.  */
+        it('login fails and error message is shown with invalid credentials',
+            function () {
+                cy.contains('Login')
+                cy.get('#usrInput').type('SanTheBlogger')
+                cy.get('#pwdInput').type('V‰‰r‰Salis')
+                cy.get('#loginBtn').click()
+                cy.get('.errorMsg').
+                    should('contain', 'Invalid username or password').
+                    and('have.css', 'color', 'rgb(139, 0, 0)').
+                    and('have.css', 'border-style', 'solid')
+                cy.contains('Currently logged in as Sampson.').should('not.exist')
+            })
+
+    })
+})

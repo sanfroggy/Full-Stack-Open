6c1c26b2f32cfde9f84f47e7b1a2c0d13dd9b515
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-10-17 21:46:38 +0300

    Update blog_app.cy.js

--- part5/part5.17/cypress/e2e/blog_app.cy.js
+++ part5/part5.17/cypress/e2e/blog_app.cy.js
@@ -1,10 +1,15 @@
 describe('When blog app frontend is visited', function () {
 
+    /*Emptying the database from User and Blog objects and
+    visiting the "http://localhost:3000" defined in the configuration 
+    file as baseUrl, before each test. */
     beforeEach(function () {
         cy.request('POST', 'http://localhost:3001/api/testing/reset')
         cy.visit('/')
     })
 
+    /*Testing that the login form is shown by default when the frontend
+    url is visited. */
     it('login form is shown by default', function() {
         cy.contains('Login')
         cy.contains('Log in to application:')

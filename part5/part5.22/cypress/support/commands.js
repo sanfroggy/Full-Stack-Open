6f42492ba27e56d5c417a9e6e37288cd7444782d
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-10-18 04:02:34 +0300

    Add files via upload

---
+++ part5/part5.22/cypress/support/commands.js
@@ -0,0 +1,56 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+Cypress.Commands.add('login', ({ username, password }) => {
+    cy.request('POST', 'http://localhost:3001/api/login'
+        , { username, password }).then(({ body }) => {
+        localStorage.setItem('loggedUserData', JSON.stringify(body))
+        cy.visit('http://localhost:3000')
+    })
+})
+
+Cypress.Commands.add('loginWithResponse', ({ username, password }) => {
+    cy.request({
+        method: 'POST', url: 'http://localhost:3001/api/login'
+        , failOnStatusCode: false, body: JSON.stringify({ username, password })
+    })
+        .then((response) => {
+            return response.status
+        })
+})
+
+Cypress.Commands.add('createBlog', ({ title, author, url }) => {
+    cy.request({
+        method: 'POST',
+        url: 'http://localhost:3001/api/blogs',
+        body: { title, author, url },
+        headers: {
+            'Authorization': `Bearer ${JSON.parse(
+                localStorage.getItem('loggedUserData')).token
+            }`
+        }
+    })
+    cy.visit('http://localhost:3000')
+})
+
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })

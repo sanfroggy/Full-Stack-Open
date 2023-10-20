618e8c5b619e70445da0cacc77102c9231226deb
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-10-21 01:25:32 +0300

    Update anecdotes.js

--- part6/part6.14/src/services/anecdotes.js
+++ part6/part6.14/src/services/anecdotes.js
@@ -4,6 +4,8 @@ import axios from 'axios'
 //Defining a baseUrl to communicate with the JSON server.
 const baseUrl = 'http://localhost:3001/anecdotes'
 
+/*Defining a function to return all anecdotes contained by 
+db.json through the JSON server. */
 const getAll = async () => {
     const response = await axios.get(baseUrl)
     return response.data

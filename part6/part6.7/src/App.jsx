15ecadbec58a054083c2cc24b2168f9e8fa920e7
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-10-20 05:32:24 +0300

    Add files via upload

---
+++ part6/part6.7/src/App.jsx
@@ -0,0 +1,42 @@
+/*Importing the useSelector, useDispatch hooks, "action creators" and the form
+used to create a new anecdote. */
+import { useSelector, useDispatch } from 'react-redux'
+import AnecdoteForm from './components/AnecdoteForm'
+import { voteForAnecdote, sortAnecdotes }
+    from './reducers/anecdoteReducer'
+
+const App = () => {
+
+    //Defining a variables for the list of anecdotes and the useDispatch hook.
+    const anecdotes = useSelector(state => state)
+    const dispatch = useDispatch()
+
+    //Using useDispatch hook to define a function for voting an anecdote.
+    const vote = (id) => {
+        dispatch(voteForAnecdote(id))
+        dispatch(sortAnecdotes())
+    }
+
+    //Displaying the array of anecdotes.
+    return (
+        <div>
+            <h2>Anecdotes: </h2>
+            {anecdotes.map(anecdote =>
+                <div key={anecdote.id}>
+                    <div>
+                        {anecdote.content}
+                    </div>
+                    <div>
+                        has {anecdote.votes} votes.
+                        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button onClick={() => vote(anecdote.id)}>Vote</button>
+                    </div>
+                </div>
+            )}
+            <AnecdoteForm />
+
+
+        </div>
+    )
+}
+
+export default App
---
+++ part6/part6.7/src/components/AnecdoteForm.jsx
@@ -0,0 +1,33 @@
+//Importing the useSelector, useDispatch hooks and the action creators.
+import { useDispatch } from 'react-redux'
+import { createNewAnecdote, sortAnecdotes } from '../reducers/anecdoteReducer'
+
+const AnecdoteForm = () => {
+
+    //Defining a variable for the the useDispatch hook.
+    const dispatch = useDispatch()
+
+    //Using useDispatch hook to define a function for adding an anecdote.
+    const addAnecdote = (event) => {
+
+        event.preventDefault()
+
+        const anecdoteString = event.target.anecdote.value
+        event.target.anecdote.value = ''
+        dispatch(createNewAnecdote(anecdoteString))
+        dispatch(sortAnecdotes())
+    }
+
+    return (
+        <div>
+            <h2> Create new: </h2>
+            <form onSubmit={addAnecdote}>
+                <div><input name="anecdote" /></div>
+                <br />
+                <button type="submit">Create</button>
+            </form>
+        </div>
+    )
+}
+
+export default AnecdoteForm

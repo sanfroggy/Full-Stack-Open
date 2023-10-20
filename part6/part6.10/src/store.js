5d1df4da8017522c2399d57704d257c3fb6eb964
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-10-20 21:25:53 +0300

    Update store.js

--- part6/part6.10/src/store.js
+++ part6/part6.10/src/store.js
@@ -3,8 +3,8 @@ import { configureStore } from '@reduxjs/toolkit'
 import anecdoteReducer from './reducers/anecdoteReducer'
 import filterReducer from './reducers/filterReducer'
 
-/*Defining a store with configureStore and the imported
-reducers and exporting the store. */
+/*Defining a store for storing states with configureStore 
+and the imported reducers and exporting the store. */
 const store = configureStore({
     reducer: {
         anecdotes: anecdoteReducer,

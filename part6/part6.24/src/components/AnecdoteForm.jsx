3f404fd3f39b5077e785d688ed6dd348750ba8fd
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-10-21 23:05:01 +0300

    Update AnecdoteForm.jsx

--- part6/part6.24/src/components/AnecdoteForm.jsx
+++ part6/part6.24/src/components/AnecdoteForm.jsx
@@ -9,7 +9,8 @@ const AnecdoteForm = () => {
 
     /*Defining variables for useQueryClient and useMutation, that are used to
     add a newAnecdote with the createAnecdote function and to have the list update 
-    immediately after a successful operation with invalidateQueries. */
+    immediately after a successful operation with invalidateQueries. On error
+    the imported useMessageDispatch function is used to display an error message.*/
     const queryClient = useQueryClient()
     const newAnecdoteMutation = useMutation({mutationFn: createAnecdote,
         onSuccess: () => {

62ca5294b24cb5f776baadca76e3d36038bb8c89
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-10-23 02:10:59 +0300

    Add files via upload

---
+++ part7/part7.9/src/components/Blogs.jsx
@@ -0,0 +1,29 @@
+import PropTypes from 'prop-types';
+import Blog from './Blog';
+
+//Defining a Blogs component for displaying the data of all the existing blogs.
+const Blogs = (props) => {
+    return (
+        <div id="blogsContainer">
+            {props.blogs.map((blog) => (
+                <Blog
+                    key={blog.id}
+                    blog={blog}
+                    updateMethod={props.handleUpdate}
+                    loggedUser={props.currentUser}
+                    deleteMethod={props.handleDelete}
+                />
+            ))}
+        </div>
+    );
+};
+
+//Using prop-types to give a warning if necessary values are not received correctly.
+Blogs.propTypes = {
+    blogs: PropTypes.arrayOf(PropTypes.object).isRequired,
+    handleUpdate: PropTypes.func.isRequired,
+    currentUser: PropTypes.object.isRequired,
+    handleDelete: PropTypes.func.isRequired,
+};
+
+export default Blogs;

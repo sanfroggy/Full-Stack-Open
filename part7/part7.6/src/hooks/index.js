3cbc46bc716f3fd8b338a1b93088320e0fe4bf1d
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-10-22 06:38:11 +0300

    Add files via upload

---
+++ part7/part7.6/src/hooks/index.js
@@ -0,0 +1,39 @@
+//Importing useState hook.
+import { useState } from "react"
+
+/*Creating a custom hook meant for form input
+handling called useField. input type received as
+a parameter. */
+const useField = (type) => {
+
+    //Defining the value "state variable", onChange and reset functions.
+    const [value, setValue] = useState('')
+
+    //Set value variable to the value of the input field.
+    const onChange = (event) => {
+        setValue(event.target.value)
+    }
+
+    //Set the input field value to an empty string.
+    const reset = () => {
+        setValue('')
+    }
+
+
+    const objectProps = {
+        type,
+        value,
+        onChange
+    }
+
+    /*Returning all variables with onChange function and the reset function, 
+    so that the field can easily be given the required props with the 
+    spread syntax and also so that it can easily be reset. */
+    return {
+        objectProps,
+        reset
+    }
+}
+
+//Exporting the custom useField hook.
+export { useField }

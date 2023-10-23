a6e1e9846e850b9ef8b2bea410b1c742316a00ac
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-10-23 04:34:18 +0300

    Update store.js

--- part7/part7.10/src/store.js
+++ part7/part7.10/src/store.js
@@ -2,6 +2,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import notificationReducer from './reducers/notificationReducer'
 
+/*Defining a store for storing states with configureStore 
+and the imported anecdotes, filter and message reducers 
+and exporting the store. */
 const store = configureStore({
     reducer: {
         notification: notificationReducer

f0260ccce76d6f37c5d6f5d4dad952d92cddab28
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-10-31 07:12:15 +0200

    Update mutations.js

--- part8/part8.10/src/services/mutations.js
+++ part8/part8.10/src/services/mutations.js
@@ -1,6 +1,8 @@
 //Importing gql from apollo client.
 import { gql } from '@apollo/client'
 
+/*Defining a mutation used to add a book to the backend.
+The added book object is the returned. */
 export const ADD_BOOK = gql`
     mutation newBook($title: String!, $author: String!, $published: Int!, $genres: [String!]!) {
         addBook(

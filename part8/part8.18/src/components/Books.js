a94e3b42b205ee3f1a38c1f1a51733fe7345c6de
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-11-01 21:32:33 +0200

    Add files via upload

---
+++ part8/part8.18/src/components/Books.js
@@ -0,0 +1,48 @@
+/*Importing useQuery hook from apollo client abd the 
+defined ALL_BOOKS query. */
+import { useQuery } from '@apollo/client'
+import { ALL_BOOKS } from '../services/queries'
+
+/*Defining a component to display an array of books
+in an html table. */
+const Books = () => {
+
+  /*Using the useQuery hook to get all book objects 
+  from backend. */
+  const result = useQuery(ALL_BOOKS)
+
+  if (result.loading) {
+    return <div><br />Loading...<br /></div>
+  }
+
+  const books = [...result.data.allBooks]
+
+  /*Returning an html table with the title, author and year of
+  publication of each book object in books array. */
+  return (
+    <div>
+      <h2>Books:</h2>
+
+      <table>
+        <tbody>
+          <tr>
+            <th></th>
+            <th>Author:</th>
+                      <th>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;Published:</th>
+          </tr>
+          {books.map((a) => (
+            <tr key={a.title}>
+              <td>{a.title}</td>
+                  <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                      &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                      &nbsp;&nbsp;&nbsp;{a.author.name}</td>
+                  <td>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{a.published}</td>
+            </tr>
+          ))}
+        </tbody>
+        </table>
+    </div>
+  )
+}
+
+export default Books
---
+++ part8/part8.18/src/components/NewBook.js
@@ -0,0 +1,103 @@
+/*Importing the useState and useMutation hooks. Also importing
+the necessary mutations and queries. */
+import { useMutation } from '@apollo/client'
+import { ADD_BOOK } from '../services/mutations'
+import { useState } from 'react'
+import { ALL_BOOKS, ALL_AUTHORS } from '../services/queries'
+
+/*Defining a component to display a form used to 
+create a new book. */
+const NewBook = ({ setMessage }) => {
+
+  /*Creating state variables to contain the desired
+  values of the book object being created. */
+  const [title, setTitle] = useState('')
+  const [author, setAuthor] = useState('')
+  const [published, setPublished] = useState('')
+  const [genre, setGenre] = useState('')
+  const [genres, setGenres] = useState([])
+
+  /*Defining a variable for the useMutation hook and making it
+  also refetch the books and authors, when used to add a new book.
+  If the query results in an error an appropriate error message is shown. */
+  const [createBook] = useMutation(ADD_BOOK, {
+      refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }],
+      onError: (error) => {
+          setMessage(error.message)
+
+          setTimeout(() => {
+              setMessage('')
+          }, 5000)
+      }
+  })
+
+  /*Defining a method to empty the input fields
+  and reset the state variables when a new book
+  is being created. */
+  const submit = async (event) => {
+    event.preventDefault()
+
+    createBook({ variables: {title, author, published, genres} })
+
+    setTitle('')
+    setPublished('')
+    setAuthor('')
+    setGenres([])
+    setGenre('')
+  }
+
+  //Adding a genre for the book.
+  const addGenre = () => {
+    setGenres(genres.concat(genre))
+    setGenre('')
+  }
+
+  /*Returning a form with input fields and a submit button. */
+  return (
+    <div>
+        <form onSubmit={submit}>
+        <br />
+        <div>
+          Title:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input
+            value={title}
+            onChange={({ target }) => setTitle(target.value)}
+          />
+        </div>
+        <br />
+        <div>
+          Author:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input
+            value={author}
+            onChange={({ target }) => setAuthor(target.value)}
+          />
+        </div>
+        <br />
+        <div>
+          Published:&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input
+            type="number"
+            value={published}
+            onChange={({ target }) => setPublished(parseInt(target.value))}
+          />
+          </div>
+          <br />
+          <div>&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+          <input
+            value={genre}
+            onChange={({ target }) => setGenre(target.value)}
+          />
+                  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<button onClick={addGenre} type="button">
+             Add genre
+             </button>
+          </div>
+          <br />
+              <div>Genres: &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;{genres.join(', ')}</div>
+          <br />
+        <button type="submit">Create book</button>
+      </form>
+    </div>
+  )
+}
+
+export default NewBook

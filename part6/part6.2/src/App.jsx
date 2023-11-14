010fd3f40cac9dc29cf505974b3bc71650af04de
sanfroggy <47052814+sanfroggy@users.noreply.github.com>
2023-11-14 13:33:29 +0200

    Finished unicafe with Redux app.

--- part6/part6.2/src/App.jsx
+++ part6/part6.2/src/App.jsx
@@ -10,27 +10,46 @@ import reducer from './reducer'
 //Creating a store that uses the imported reduce component.
 const store = createStore(reducer)
 
-const App = () => {
+    const App = () => {
 
-    //Defining a function to use the "GOOD" action of the reducer.
-    const good = () => {
-        store.dispatch({
-            type: 'GOOD'
-        })
-    }
+        //Defining a function to use the "GOOD" action of the reducer.
+        const good = () => {
+            store.dispatch({
+                type: 'GOOD'
+            })
+        }
 
-    //Defining a function to use the "OK" action of the reducer.
-    const ok = () => {
-        store.dispatch({
-            type: 'OK'
-        })
-    }
+        //Defining a function to use the "OK" action of the reducer.
+        const ok = () => {
+            store.dispatch({
+                type: 'OK'
+            })
+        }
 
-    //Defining a function to use the "BAD" action of the reducer.
-    const bad = () => {
-        store.dispatch({
-            type: 'BAD'
-        })
+        //Defining a function to use the "BAD" action of the reducer.
+        const bad = () => {
+            store.dispatch({
+                type: 'BAD'
+            })
+        }
+
+        /*Displaying the buttons and the number of related reviews in the store
+        with the use of store.getState. */
+        return (
+            <div>
+                <button onClick={reset}>Reset stats</button>
+                <br />
+                <br />
+                <div>Good: {store.getState().good} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <button onClick={good}>Good</button></div>
+                <br />
+                <div>Ok: {store.getState().ok} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <button onClick={ok}>Ok</button></div>
+                <br />
+                <div>Bad: {store.getState().bad} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+                    <button onClick={bad}>Bad</button></div>
+            </div>
+        )
     }
 
     //Defining a function to use the "ZERO" action of the reducer.
@@ -40,29 +59,11 @@ const App = () => {
         })
     }
 
-    /*Displaying the buttons and the number of related reviews in the store
-    with the use of store.getState. */
-    return (
-        <div>      
-            <button onClick={reset}>Reset stats</button>
-            <br />
-            <br />
-            <div>Good: {store.getState().good} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <button onClick={good}>Good</button></div>
-            <br />
-            <div>Ok: {store.getState().ok} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <button onClick={ok}>Ok</button></div>
-            <br />
-            <div>Bad: {store.getState().bad} &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                <button onClick={bad}>Bad</button></div>
-        </div>
-    )
+    const root = ReactDOM.createRoot(document.getElementById('root'))
 
-const root = ReactDOM.createRoot(document.getElementById('root'))
+    const renderApp = () => {
+        root.render(<App />)
+    }
 
-const renderApp = () => {
-    root.render(<App />)
-}
-
-renderApp()
-store.subscribe(renderApp)
+    renderApp()
+    store.subscribe(renderApp)
